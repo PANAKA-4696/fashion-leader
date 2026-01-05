b01ab3bfbce706a4fa74626c22328db04549b13e
panaka <seaon.tnk@gmail.com>
2026-01-05 14:07:45 +0900

    ファイル構造の作成と、web.phpにて担当場所の作成を完了.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//リーダタナカ担当場所
+// 今日のコーデ変更保存画面
+Route::get('/main/closet-edit', [MainController::class, 'editCloset']);
+
+//フクシマ担当場所
+
+//タナカ担当場所
+
+//オオタ担当場所
+
+//モギ担当場所
+
+//クニヤス担当場所
